cf294c0611d1bac43886b0c308aa937173e9f970
Eslam Hesham <esl2mhesham@gmail.com>
2021-03-10 00:43:57 +0200

    fake pages

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./components/layout";
+export default function About() {
+	return (
+		<Layout>
+			<article className="my-4 text-3xl font-mono">
+				<h2 className="font-bold">I'M Eslam</h2>
+				<p> I love Hip-hop, Movies, Chess</p>
+			</article>
+		</Layout>
+	);
+}
---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./components/layout";
+
+export default function Login() {
+	return (
+		<Layout>
+			<article className="flex justify-center items-center h-full bg-white">
+				<p className="text-3xl my-4 "> Pleas Login First!</p>
+			</article>
+		</Layout>
+	);
+}
